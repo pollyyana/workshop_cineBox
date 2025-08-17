a4187e2e7c7d9c7df570075316fa3bfed157c0a3
pollyyana <pollyana.medeirosk@gmail.com>
2025-08-17 14:52:04 -0300

    rest client

--- cinebox/lib/ui/core/themes/resource.dart
+++ cinebox/lib/ui/core/themes/resource.dart
@@ -4,44 +4,44 @@
 class R {
   const R._();
 
-  /// ![preview](file:///home/ponto/flutter_experience/cinebox/assets/images/bg_login.png)
+  /// ![preview](file:///home/ponto/workshop_cineBox/cinebox/assets/images/bg_login.png)
   static const String assetsImagesBgLoginPng = 'assets/images/bg_login.png';
 
-  /// ![preview](file:///home/ponto/flutter_experience/cinebox/assets/images/google_logo.png)
+  /// ![preview](file:///home/ponto/workshop_cineBox/cinebox/assets/images/google_logo.png)
   static const String assetsImagesGoogleLogoPng =
       'assets/images/google_logo.png';
 
-  /// ![preview](file:///home/ponto/flutter_experience/cinebox/assets/images/icon_filme_nav_bar.png)
+  /// ![preview](file:///home/ponto/workshop_cineBox/cinebox/assets/images/icon_filme_nav_bar.png)
   static const String assetsImagesIconFilmeNavBarPng =
       'assets/images/icon_filme_nav_bar.png';
 
-  /// ![preview](file:///home/ponto/flutter_experience/cinebox/assets/images/icon_filme_nav_bar_inativo.png)
+  /// ![preview](file:///home/ponto/workshop_cineBox/cinebox/assets/images/icon_filme_nav_bar_inativo.png)
   static const String assetsImagesIconFilmeNavBarInativoPng =
       'assets/images/icon_filme_nav_bar_inativo.png';
 
-  /// ![preview](file:///home/ponto/flutter_experience/cinebox/assets/images/icon_heart_bnb.png)
+  /// ![preview](file:///home/ponto/workshop_cineBox/cinebox/assets/images/icon_heart_bnb.png)
   static const String assetsImagesIconHeartBnbPng =
       'assets/images/icon_heart_bnb.png';
 
-  /// ![preview](file:///home/ponto/flutter_experience/cinebox/assets/images/icon_heart_bnb_inativo.png)
+  /// ![preview](file:///home/ponto/workshop_cineBox/cinebox/assets/images/icon_heart_bnb_inativo.png)
   static const String assetsImagesIconHeartBnbInativoPng =
       'assets/images/icon_heart_bnb_inativo.png';
 
-  /// ![preview](file:///home/ponto/flutter_experience/cinebox/assets/images/icon_profile_nav_bar.png)
+  /// ![preview](file:///home/ponto/workshop_cineBox/cinebox/assets/images/icon_profile_nav_bar.png)
   static const String assetsImagesIconProfileNavBarPng =
       'assets/images/icon_profile_nav_bar.png';
 
-  /// ![preview](file:///home/ponto/flutter_experience/cinebox/assets/images/icon_profile_nav_bar_inativo.png)
+  /// ![preview](file:///home/ponto/workshop_cineBox/cinebox/assets/images/icon_profile_nav_bar_inativo.png)
   static const String assetsImagesIconProfileNavBarInativoPng =
       'assets/images/icon_profile_nav_bar_inativo.png';
 
-  /// ![preview](file:///home/ponto/flutter_experience/cinebox/assets/images/logo.png)
+  /// ![preview](file:///home/ponto/workshop_cineBox/cinebox/assets/images/logo.png)
   static const String assetsImagesLogoPng = 'assets/images/logo.png';
 
-  /// ![preview](file:///home/ponto/flutter_experience/cinebox/assets/images/no-image.png)
+  /// ![preview](file:///home/ponto/workshop_cineBox/cinebox/assets/images/no-image.png)
   static const String assetsImagesNoImagePng = 'assets/images/no-image.png';
 
-  /// ![preview](file:///home/ponto/flutter_experience/cinebox/assets/images/small_banner.png)
+  /// ![preview](file:///home/ponto/workshop_cineBox/cinebox/assets/images/small_banner.png)
   static const String assetsImagesSmallBannerPng =
       'assets/images/small_banner.png';
 }
